2ceac4b64d16db974a4179e88e1281af6d064ebd
tomzoo <justdamn@sina.com>
2018-09-06 19:14:18 +0800

    是
    是

---
+++ animate.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    var animateButton = function(e) {
+        e.preventDefault;
+        // reset animation 复位动画
+        e.target.classList.remove('animate');
+        e.target.classList.add('animate');
+        setTimeout(function() {
+            e.target.classList.remove('animate');
+        }, 700);
+    };
+
+    var btn = document.getElementsByClassName("bubbly-button");
+    for (var i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', animateButton, false);
+    }
+}
