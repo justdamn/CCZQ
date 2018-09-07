ed233de4e75acbb076c004b7115f26e771ff61ac
tomzoo <justdamn@sina.com>
2018-09-07 16:50:01 +0800

    身份证
    身份证

--- animate.js
+++ animate.js
@@ -1,16 +1,14 @@
-window.onload = function () {
-    var animateButton = function(e) {
-        e.preventDefault;
-        // reset animation 复位动画
+var animateButton = function (e) {
+    e.preventDefault;
+    // reset animation 复位动画
+    e.target.classList.remove('animate');
+    e.target.classList.add('animate');
+    setTimeout(function () {
         e.target.classList.remove('animate');
-        e.target.classList.add('animate');
-        setTimeout(function() {
-            e.target.classList.remove('animate');
-        }, 700);
-    };
+    }, 700);
+};
 
-    var btn = document.getElementsByClassName("bubbly-button");
-    for (var i = 0; i < btn.length; i++) {
-        btn[i].addEventListener('click', animateButton, false);
-    }
+var btn = document.getElementsByClassName("bubbly-button");
+for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', animateButton, false);
 }
